dc3f25e81ae96eda180a7501b9c87dca30bccb14
electro9666 <electro9666@gmail.com>
2021-08-07 22:37:43 +0900

    add override

--- spring_security2/src/main/java/com/tistory/pentode/AdminController.java
+++ spring_security2/src/main/java/com/tistory/pentode/AdminController.java
@@ -2,15 +2,22 @@ package com.tistory.pentode;
 
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Locale;
+
+import javax.servlet.Filter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +26,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class AdminController {
 	private static final Logger logger = LoggerFactory.getLogger(AdminController.class);
+
+	@Autowired
+	@Qualifier("springSecurityFilterChain")
+	Filter springSecurityFilterChain;
 
 	@RequestMapping(value = "/admin/adminHome.do", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
@@ -44,6 +55,21 @@ public class AdminController {
 			e.printStackTrace();
 		}
 
+		return "admin/adminHome";
+	}
+
+	@RequestMapping(value = "/admin/test.do", method = RequestMethod.GET)
+	public String test(Locale locale, Model model) {
+		FilterChainProxy filterChainProxy = (FilterChainProxy) springSecurityFilterChain;
+		List<SecurityFilterChain> list = filterChainProxy.getFilterChains();
+		for (int i = 0; i < list.size(); i++) {
+			System.out.println("i:" + i);
+			SecurityFilterChain securityFilterChain = list.get(i);
+			List<Filter> filters = securityFilterChain.getFilters();
+			for (int j = 0; j < filters.size(); j++) {
+				System.out.println(">>>>>> j: " + j + ": " + filters.get(j).getClass());
+			}
+		}
 		return "admin/adminHome";
 	}
 }
